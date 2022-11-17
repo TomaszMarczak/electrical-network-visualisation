814046c977419dcbad84b8855168e2e2882f6bd6
Tomasz Marczak <tommy.marczak@gmail.com>
2022-11-17 10:34:14 +0800

    Location editing

---
+++ src/components/EditLocationForm.tsx
@@ -0,0 +1,99 @@
+import { Form, Button, Modal } from "react-bootstrap";
+import { useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useProjectAssets } from "../contexts/ProjectAssetsContext";
+
+type EditModalFormTypes = {
+  show: boolean;
+  hide: () => void;
+  id: string;
+};
+
+export default function EditLocationForm({
+  show,
+  hide,
+  id,
+}: EditModalFormTypes) {
+  const { locations, setLocations, pushAlert } = useProjectAssets();
+  const location = locations.find((location) => location.id === id);
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const areaRef = useRef<HTMLInputElement | null>(null);
+  const detailsRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (location?.id && nameRef.current?.value && areaRef.current?.value) {
+      const editedLocation = {
+        id: location.id,
+        name: nameRef.current.value,
+        area: areaRef.current.value,
+        details: detailsRef.current?.value || null,
+      };
+      setLocations(
+        locations.map((prevLocation) => {
+          if (prevLocation.id !== editedLocation.id) return prevLocation;
+          else return editedLocation;
+        })
+      );
+
+      pushAlert("success", `Location edited!`);
+      hide();
+    }
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={hide}
+      size="lg"
+      aria-labelledby="Modal Add Location"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="Modal Add Device">Edit location</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="d-flex flex-column p-3 p-3">
+          <Form
+            onSubmit={handleSubmit}
+            className="d-flex flex-column justify-content-between gap-2 flex-wrap align-items center gap-1"
+          >
+            <div className="d-flex justify-content-start gap-3 flex-wrap">
+              <Form.Group className="flex-grow-1">
+                <Form.Label>Location name:</Form.Label>
+                <Form.Control
+                  defaultValue={location?.name}
+                  ref={nameRef}
+                  type="text"
+                  placeholder="Enter location name..."
+                />
+              </Form.Group>
+              <Form.Group className="flex-grow-1">
+                <Form.Label>Area name:</Form.Label>
+                <Form.Control
+                  defaultValue={location?.area}
+                  ref={areaRef}
+                  type="text"
+                  placeholder="Floor, deck, zone, etc..."
+                />
+              </Form.Group>
+              <Form.Group className="w-75">
+                <Form.Label>Details:</Form.Label>
+                <Form.Control
+                  defaultValue={location?.details || ""}
+                  ref={detailsRef}
+                  type="text"
+                  placeholder="More details about your location..."
+                />
+              </Form.Group>
+
+              <Button type="submit" className="ms-auto mt-auto">
+                Edit location
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
--- src/components/LocationsTable.tsx
+++ src/components/LocationsTable.tsx
@@ -1,67 +1,89 @@
 import { Button, Table, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { BsTrash } from "react-icons/bs";
+import { BsPencilFill, BsTrash } from "react-icons/bs";
 import { useProjectAssets } from "../contexts/ProjectAssetsContext";
+import { useState } from "react";
+import EditLocationForm from "./EditLocationForm";
 
 export default function LocationsTable() {
   const { locations, setLocations, pushAlert } = useProjectAssets();
+  const [editLocationModalShow, setEditLocationModalShow] =
+    useState<string>("");
 
   const handleDelete = (locationId: string) => {
     setLocations(locations.filter((location) => location.id !== locationId));
   };
+
   return (
     <>
       {locations.length > 0 ? (
-        <Table
-          hover
-          size="sm"
-          className="my-1"
-          responsive
-          style={{
-            verticalAlign: "middle",
-          }}
-        >
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Area</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {locations.map((location) => {
-              return (
-                <tr key={location.id}>
-                  <OverlayTrigger
-                    placement="left"
-                    delay={{ show: 400, hide: 100 }}
-                    overlay={
-                      location.details ? (
-                        <Tooltip>{location.details}</Tooltip>
-                      ) : (
-                        <></>
-                      )
-                    }
-                  >
-                    <td>{location.name}</td>
-                  </OverlayTrigger>
-                  <td>{location.area}</td>
-                  <td className="text-end">
-                    <Button
-                      size="sm"
-                      variant="outline-danger"
-                      onClick={() => {
-                        handleDelete(location.id);
-                        pushAlert("danger", "Location deleted!");
-                      }}
+        <>
+          <Table
+            hover
+            size="sm"
+            className="my-1"
+            responsive
+            style={{
+              verticalAlign: "middle",
+            }}
+          >
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Area</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {locations.map((location) => {
+                return (
+                  <tr key={location.id}>
+                    <OverlayTrigger
+                      placement="left"
+                      delay={{ show: 400, hide: 100 }}
+                      overlay={
+                        location.details ? (
+                          <Tooltip>{location.details}</Tooltip>
+                        ) : (
+                          <></>
+                        )
+                      }
                     >
-                      <BsTrash />
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+                      <td>{location.name}</td>
+                    </OverlayTrigger>
+                    <td>{location.area}</td>
+                    <td className="text-end">
+                      <Button
+                        size="sm"
+                        variant="outline-secondary"
+                        className="me-1"
+                        onClick={() => {
+                          setEditLocationModalShow(location.id);
+                        }}
+                      >
+                        <BsPencilFill />
+                      </Button>
+                      <Button
+                        size="sm"
+                        variant="outline-danger"
+                        onClick={() => {
+                          handleDelete(location.id);
+                          pushAlert("danger", "Location deleted!");
+                        }}
+                      >
+                        <BsTrash />
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <EditLocationForm
+            show={editLocationModalShow ? true : false}
+            hide={() => setEditLocationModalShow("")}
+            id={editLocationModalShow}
+          />
+        </>
       ) : (
         <div className="m-auto">Create your first location...</div>
       )}
